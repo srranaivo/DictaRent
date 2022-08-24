98ab3594fa2f249b861b4c5f52a77d20f40fe6bf
sophie ramahenina <ramahenina.sophie@gmail.com>
2022-08-24 11:08:04 +0200

    attrubution d'une land periode à une land

--- app/controllers/land_periodes_controller.rb
+++ app/controllers/land_periodes_controller.rb
@@ -14,7 +14,8 @@ class LandPeriodesController < ApplicationController
   def create
     puts "create methode"
     @land_periodes = LandPeriode.new(land_periodes_params)
-    @land_periodes.land_id = current_user.land
+    @land_periodes.land = current_user.land
+
     if @land_periodes.save
       redirect_to land_periodes_path
     else
@@ -22,25 +23,10 @@ class LandPeriodesController < ApplicationController
     end
   end
 
-private
+  private
 
-def land_periodes_params
-  params.require(:land_periode).permit(:starts_the, :ends_the)
-end
-
-#########
-# t.datetime "starts_the"
-# t.datetime "ends_the"
-# t.bigint "land_id"
-# t.datetime "created_at", null: false
-# t.datetime "updated_at", null: false
-# t.index ["land_id"], name: "index_land_periodes_on_land_id"
-
-
-########
-
-
-
-
+  def land_periodes_params
+    params.require(:land_periode).permit(:starts_the, :ends_the, :land_id)
+  end
 
 end
