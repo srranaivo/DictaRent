cb6f2ff67b245d1a157383f7d91e571ff475a69b
Aurelien Pellegrini <aurelien.pellegrini@gmail.com>
2022-08-26 11:54:36 +0200

    change email to name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates :name, presence: true
+
   def dictator?
     return current_user.land ? true : false
   end
